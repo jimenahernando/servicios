f1c67471b6fefb8badc365ce4fc2d5ed3e17386c
jimena <jimenahernando@hotmail.com>
2021-08-23 23:58:23 +0200

    Componente appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Persona } from './interfaces/persona.interface';
+import { PersonasService } from './services/personas.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'servicios';
+
+  persona: Persona;
+  id:number;
+
+  constructor( private personasService: PersonasService){
+    this.persona = {
+      id: 0,
+      nombre: '',
+      edad: '',
+      email: ''
+    };
+    this.id = 1;
+  }
+  
+  recogerDatosForm(pForm: any){
+    this.persona = pForm.value;
+    // this.persona.id = this.id;
+    this.persona.id = this.id++;
+    this.personasService.addPerson(this.persona);
+    // this.id++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaPersonajesComponent } from './components/lista-personajes/lista-personajes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPersonajesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
